df548ea967fb1d5b550d467e19a42713757c51e0
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-10-21 15:16:24 +0200

    Correction

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,3 +1,23 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
 // Jeu du plus ou moins
 // 1/ Ecrire une fonction qui génère un entier aléatoire entre 0 et 100 (API Math sur MDN)
 // 2/ Demander et récupérer la saisie (API Readline sur Node.js) puis afficher si le nombre est plus grand, plus petit ou trouvé
@@ -7,24 +27,43 @@
 
 const readline = require('node:readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà saisi : ' + essais.join(', '))
+  }
+
   rl.question('Quel est le nombre ? ', (answer) => {
     // Attention answer est de type string
     console.log('Vous avez saisi : ' + answer);
 
-    // Pour rejouer :
-    jouer();
+    const entierSaisi = Number.parseInt(answer, 10);
 
-    // A faire quand on trouvé le bon nombre :
-    // rl.close();
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Erreur: la saisie n\'est pas un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
 
 // pile d'appels
